8516ee490048cf9c1a1c81ab2e1b1bdaa02295c8
ChoMosigi <whdbsqja1@gmail.com>
2023-06-30 13:43:16 +0900

    ShopReview mapper generate

--- src/main/java/project/com/hotplace/shopreview/controller/ShopReviewController.java
+++ src/main/java/project/com/hotplace/shopreview/controller/ShopReviewController.java
@@ -1,5 +1,44 @@
 package project.com.hotplace.shopreview.controller;
 
-public class ShopReviewController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+public class ShopReviewController {
+	/*
+	@Autowired
+	CommentsService commService;
+
+	@RequestMapping(value = "/c_updateOK.do", method = RequestMethod.GET)
+	public String updateOK(ShopReviewVO vo) {
+		log.info("/c_updateOK.do...{}", vo);
+		
+		int result = commService.update(vo);
+		log.info("result...{}", result);
+		
+		return "redirect:b_selectOne.do?wnum="+vo.getWnum();
+		
+	}
+	
+	@RequestMapping(value = "/c_deleteOK.do", method = RequestMethod.GET)
+	public String deleteOK(CommentsVO vo) {
+		log.info("/c_deleteOK.do...{}", vo);
+		
+		int result = commService.delete(vo);
+		log.info("result...{}", result);
+		
+		return "redirect:b_selectOne.do?wnum="+vo.getWnum();
+		
+	}
+	
+	@RequestMapping(value = "/c_insertOK.do", method = RequestMethod.GET)
+	public String insertOK(CommentsVO vo) {
+		log.info("/c_insertOK.do...{}", vo);
+		
+		int result = commService.insert(vo);
+		log.info("result...{}", result);
+		
+		return "redirect:b_selectOne.do?wnum="+vo.getWnum();
+		
+	}
+	*/
 }
--- src/main/java/project/com/hotplace/shopreview/model/ShopReviewDAOimpl.java
+++ src/main/java/project/com/hotplace/shopreview/model/ShopReviewDAOimpl.java
@@ -23,28 +23,28 @@ public class ShopReviewDAOimpl implements ShopReviewDAO {
 	public List<ShopReviewVO> selectAll(ShopReviewVO vo) {
 		log.info("selectAll()...");
 
-		return sqlSession.selectList("SELECT_ALL");
+		return sqlSession.selectList("SRE_SELECT_ALL");
 	}
 
 	@Override
 	public int insert(ShopReviewVO vo) {
 		log.info("insert()...");
 		
-		return sqlSession.insert("INSERT", vo);
+		return sqlSession.insert("SRE_INSERT", vo);
 	}
 
 	@Override
 	public int update(ShopReviewVO vo) {
 		log.info("update()...");
 		
-		return sqlSession.update("UPDATE", vo);
+		return sqlSession.update("SRE_UPDATE", vo);
 	}
 
 	@Override
 	public int delete(ShopReviewVO vo) {
 		log.info("delete()...");
 		
-		return sqlSession.delete("DELETE", vo);
+		return sqlSession.delete("SRE_DELETE", vo);
 	}
 
 }
